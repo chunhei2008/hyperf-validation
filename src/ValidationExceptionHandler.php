92311d070002acebb14bbe39c54991e675850863
wangyi <chunhei2008@qq.com>
2019-07-26 17:33:38 +0800

    feat fix respose => response

--- src/ValidationExceptionHandler.php
+++ src/ValidationExceptionHandler.php
@@ -20,28 +20,20 @@ class ValidationExceptionHandler extends ExceptionHandler
 {
     public function handle(Throwable $throwable, ResponseInterface $response)
     {
-        // 判断被捕获到的异常是希望被捕获的异常
         if ($throwable instanceof ValidationException) {
-            // 格式化输出
             $data = json_encode([
-                'code' => $throwable->getCode(),
+                'code'    => $throwable->getCode(),
                 'message' => $throwable->validator->errors()->first(),
             ], JSON_UNESCAPED_UNICODE);
 
-            // 阻止异常冒泡
             $this->stopPropagation();
+
             return $response->withStatus(500)->withBody(new SwooleStream($data));
         }
 
-        // 交给下一个异常处理器
-        return $respose;
-
-        // 或者不做处理直接屏蔽异常
+        return $response;
     }
 
-    /**
-     * 判断该异常处理器是否要对该异常进行处理
-     */
     public function isValid(Throwable $throwable): bool
     {
         return true;
